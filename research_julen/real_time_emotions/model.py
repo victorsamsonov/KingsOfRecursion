7bd049b3964d66ebcd9be2b3099ee366823cf74f
jferro <jferro@cccis.com>
2023-10-22 07:20:23 -0400

    Last push, copying reserarch folder to main

--- research_julen/real_time_emotions/model.py
+++ research_julen/real_time_emotions/model.py
@@ -14,7 +14,7 @@ class FacialExpressionModel(object):
 
     EMOTIONS_LIST = ["Angry", "Disgust",
                      "Fear", "Happy",
-                     "Neutral", "Sad",
+                     "Neutral", "Didn't understand",
                      "Surprise"]
 
     def __init__(self, model_json_file, model_weights_file):
